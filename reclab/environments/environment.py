7fb12fa8b7d6530e051c4092be6a6e130dd4878a
Karl Krauth <karl.krauth@gmail.com>
2019-10-15 19:55:24 -0700

    More work on the environment interface.

--- reclab/environments/environment.py
+++ reclab/environments/environment.py
@@ -1,6 +1,23 @@
 import abc
 
+
 class Environment(abc.ABC):
+    def reset(self):
+        """Reset the environment to its original state. Must be called before the first step.
+
+        Returns
+        -------
+        users : iterable
+            New users and users whose information got updated this timestep.
+        items : iterable
+            New items and items whose information got updated this timestep.
+        ratings : iterable
+            New ratings and ratings whose information got updated this timestep.
+        info : dict
+            Extra information that can be used for debugging but should not be made accessible to
+            the recommender.
+        """
+
     def step(self, recommendations):
         """Run one timestep of the environment.
 
@@ -56,3 +73,27 @@ class Environment(abc.ABC):
             All items in the environment.
         """
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def all_ratings(self):
+        """Return all ratings that have been made in the environment.
+
+        Returns
+        -------
+        ratings : iterable
+            All ratings in the environment.
+        """
+        raise NotImplementedError
+
+    def seed(self, seed=None):
+        """Set the seed the seed for this environment's random number generator(s)."""
+        pass
+
+    def close(self):
+        """Perform any necessary cleanup."""
+        pass
+
+    def __exit__(self, *args):
+        self.close()
+        return False
+

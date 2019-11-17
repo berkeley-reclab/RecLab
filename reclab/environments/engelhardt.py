96240e179cfa32e07414e96e237e6b1ca75ba3b4
Alex Zhao <axyzhao@berkeley.edu>
2019-11-17 15:16:27 -0800

    fix linting issues

--- reclab/environments/engelhardt.py
+++ reclab/environments/engelhardt.py
@@ -1,38 +1,54 @@
+"""
+Contains the implementation for the Engelhardt environment from the algorithmic confounding paper.
+
+In this environment users have a hidden preference for each topic and each item has a
+hidden topic assigned to it.
+"""
+
 import numpy as np
 import scipy
 import scipy.special
 
 from . import environment
 
-"""
-Implementation of "How How Algorithmic Confounding in Recommendation Systems Increases Homogeneity and Decreases Utility" by Chaney, Stewart, and Engelhardt (2018).
-"""
 
 class User(object):
-    def __init__(self, num_topics, known_util_weight, user_topic_weights, beta_var):
+    """Create custom User object for use in Engelhardt environment."""
+
+    def __init__(self, num_topics, known_weight,
+                 user_topic_weights, beta_var):
+        """Initialize user with features and known/unknown utility weight."""
         self.num_topics = num_topics
-        alpha = ((1 - known_util_weight) / (beta_var ** 2) - (1 / known_util_weight)) * (known_util_weight ** 2)
-        beta = alpha * ((1 / known_util_weight) - 1)
-        self.known_util_weight = np.random.beta(alpha, beta)
+        alpha = ((1 - known_weight) / (beta_var ** 2) - (1 / known_weight)) * (known_weight ** 2)
+        beta = alpha * ((1 / known_weight) - 1)
+        self.known_weight = np.random.beta(alpha, beta)
         self.preferences = np.random.dirichlet(user_topic_weights)
 
-    # returns true utility and known utility
     def rate(self, item_attributes):
+        """Return true utility and known utility from user."""
         true_util = np.dot(self.preferences, item_attributes) * 5
-        return true_util, true_util * self.known_util_weight
+        return true_util, true_util * self.known_weight
+
 
 class Engelhardt(environment.DictEnvironment):
-    def __init__(self, num_topics, num_users, num_items,
-                 rating_frequency=0.2, num_init_ratings=0, known_util_weight=0.98, beta_var = 10 ** -5):
+    """
+    Implementation of environment with known and unknown user utility, static over time.
 
-        self.known_util_weight = known_util_weight
+    Based on "How Algorithmic Confounding in Recommendation Systems Increases Homogeneity
+    and Decreases Utility" by Chaney, Stewart, and Engelhardt (2018).
+    """
+
+    def __init__(self, num_topics, num_users, num_items, rating_frequency=0.2,
+                 num_init_ratings=0, known_weight=0.98, beta_var=10 ** -5):
+        """Create an Engelhardt environment."""
+        self.known_weight = known_weight
         self.beta_var = beta_var
         self.user_topic_weights = scipy.special.softmax(np.random.rand(num_topics))
         self.item_topic_weights = scipy.special.softmax(np.random.rand(num_topics))
         self._num_topics = num_topics
         self._num_users = num_users
         self._num_items = num_items
-        self._rating_frequency=rating_frequency
+        self._rating_frequency = rating_frequency
         self._num_init_ratings = num_init_ratings
         self._users = None
         self._users_full = None
@@ -59,11 +75,15 @@ class Engelhardt(environment.DictEnvironment):
             The initial ratings where util[i, 0] corresponds to the id of the user that
             made the rating, util[i, 1] corresponds to the id of the item that was rated
             and util[i, 2] is the true utility given of the interaction.
+
         """
-        self._users_full = {user_id: User(self._num_topics, self.known_util_weight, self.user_topic_weights, self.beta_var)
-                         for user_id in range(self._num_users)}
-        self._users = {user_id: self._users_full[user_id].preferences for user_id in range(self._num_users)}
-        self._items = {item_id: np.random.dirichlet(self.item_topic_weights) for item_id in range(self._num_items)}
+        self._users_full = {user_id: User(self._num_topics, self.known_weight,
+                                          self.user_topic_weights, self.beta_var)
+                            for user_id in range(self._num_users)}
+        self._users = {user_id: self._users_full[user_id].preferences
+                       for user_id in range(self._num_users)}
+        self._items = {item_id: np.random.dirichlet(self.item_topic_weights)
+                       for item_id in range(self._num_items)}
         # self._util = scipy.sparse.dok_matrix((self._num_users, self._num_items))
 
     def _rate_item(self, user_id, item_id):
@@ -80,6 +100,7 @@ class Engelhardt(environment.DictEnvironment):
         -------
         rating : int
             The rating the item was given by the user.
+
         """
         item_attr = self._items[item_id]
         util, rating = self._users_full[user_id].rate(item_attr)

bdae77c1e04d05fe12be6bbc4499b47cb8202fc4
Karl Krauth <karl.krauth@gmail.com>
2020-02-14 18:28:43 -0800

    Moved the item_knn file to knn_recommender.

--- reclab/recommenders/__init__.py
+++ reclab/recommenders/__init__.py
@@ -1,4 +1,4 @@
 """A set of recommender to be used in conjunction with environments."""
-from .item_knn import ItemKNN
+from .knn_recommender import KNNRecommender
 from .libfm import LibFM
 from .top_pop import TopPop

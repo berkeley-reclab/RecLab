2995ac155515b38da376078edfa081eacf8bfea2
Alex Zhao <axyzhao@berkeley.edu>
2020-03-09 20:14:31 -0700

    add style changes and update requirements

--- reclab/recommenders/autorec/autorec.py
+++ reclab/recommenders/autorec/autorec.py
@@ -10,56 +10,56 @@ from .. import recommender
 
 
 class Autorec(recommender.PredictRecommender):
-    """Auto-encoders meet collaborative filtering."""
+    """Auto-encoders meet collaborative filtering.
+
+    Parameters
+    ---------
+    num_users : int
+        Number of users in the environment.
+
+    num_items : int
+        Number of items in the environment.
+
+    ratings : np.matrix
+        Matrix of shape (num_users, num_items) populated with user ratings.
+
+    hidden_neuron : int
+        Output dimension of hidden neuron.
+
+    lambda_value : float
+        Coefficient for regularization while training layers.
+
+    train_epoch : int
+        Number of epochs to train for each call.
+
+    batch_size : int
+        Batch size during initial training phase.
+
+    optimizer_method : str
+        Optimizer for training model; either Adam or RMSProp.
+
+    grad_clip : bool
+        Set to true to clip gradients to [-5, 5].
+
+    base_lr : float
+        Base learning rate for optimizer.
+
+    decay_epoch_step : int
+        Number of epochs before the optimizer decays the learning rate.
+
+    random_seed : int
+        Random seed to reproduce results.
+
+    display_step : int
+        Number of training steps before printing display text.
+
+    """
 
     def __init__(self, num_users, num_items, ratings=None,
                  hidden_neuron=50, lambda_value=1, train_epoch=100, batch_size=100,
                  optimizer_method='Adam', grad_clip=False, base_lr=1e-4, decay_epoch_step=50,
                  random_seed=1000, display_step=1):
-
-        """
-        Parameters
-        ---------
-        num_users : int
-            Number of users in the environment.
-
-        num_items : int
-            Number of items in the environment.
-
-        ratings : np.matrix
-            Matrix of shape (num_users, num_items) populated with user ratings.
-
-        hidden_neuron : int
-            Output dimension of hidden neuron.
-
-        lambda_value : float
-            Coefficient for regularization while training layers.
-
-        train_epoch : int
-            Number of epochs to train for each call.
-
-        batch_size : int
-            Batch size during initial training phase.
-
-        optimizer_method : str
-            Optimizer for training model; either Adam or RMSProp.
-
-        grad_clip : bool
-            Set to true to clip gradients to [-5, 5].
-
-        base_lr : float
-            Base learning rate for optimizer.
-
-        decay_epoch_step : int
-            Number of epochs before the optimizer decays the learning rate.
-
-        random_seed : int
-            Random seed to reproduce results.
-
-        display_step : int
-            Number of training steps before printing display text.
-
-        """
+        """Create new Autorec recommender."""
         super().__init__()
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -71,13 +71,13 @@ class Autorec(recommender.PredictRecommender):
                              hidden_neuron, lambda_value, train_epoch, batch_size, optimizer_method,
                              grad_clip, base_lr, decay_epoch_step, random_seed, display_step)
 
-    def _predict(self, user_item, round_rat=False):
+    def _predict(self, user_item, round_rat=False): # noqa: D102
         estimate = self.model.predict(user_item)
         if round_rat:
             estimate = estimate.astype(int)
         return estimate
 
-    def reset(self, users=None, items=None, ratings=None):
+    def reset(self, users=None, items=None, ratings=None): # noqa: D102
         rating_matrix = np.zeros(shape=(len(users), len(items)))
         for user_item in ratings:
             rating_matrix[user_item[0]][user_item[1]] = ratings[user_item][0]
@@ -88,7 +88,7 @@ class Autorec(recommender.PredictRecommender):
         self.model.run()
         super().reset(users, items, ratings)
 
-    def update(self, users=None, items=None, ratings=None):
+    def update(self, users=None, items=None, ratings=None): # noqa: D102
         super().update(users, items, ratings)
         for user in users:
             self.model.seen_users.add(user)

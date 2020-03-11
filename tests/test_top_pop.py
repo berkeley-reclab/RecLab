c18aa0114961c709a44cc1b16dd5d0f3e64265ec
Karl Krauth <karl.krauth@gmail.com>
2020-03-11 12:29:33 -0700

    Add test for multistep top_pop.

--- tests/test_top_pop.py
+++ tests/test_top_pop.py
@@ -29,3 +29,31 @@ def test_top_pop_one_step():
     assert recs[0, 0] == 2
     assert recs[1, 0] == 0
     assert recs[2, 0] == 0
+
+
+def test_top_pop_multi_step():
+    users = {0: np.array(0),
+             1: np.array(0)}
+    items = {0: np.array(0),
+             1: np.array(0),
+             2: np.array(0)}
+    ratings = {(0, 0): (5, np.array(0)),
+               (1, 1): (3, np.array(0))}
+    user_contexts = collections.OrderedDict([(0, np.array(0)),
+                                             (1, np.array(0))])
+
+    model = TopPop()
+    model.reset(users, items, ratings)
+    recs, _ = model.recommend(user_contexts, 1)
+    assert recs.shape == (2, 1)
+    assert recs[0, 0] == 1
+    assert recs[1, 0] == 0
+    user_contexts[2] = np.array(0)
+    model.update(users={2: np.array(0)},
+                 ratings={(0, 1): (5, np.array(0)),
+                          (1, 0): (1, np.array(0))})
+    recs, _ = model.recommend(user_contexts, 1)
+    assert recs.shape == (3, 1)
+    assert recs[0, 0] == 2
+    assert recs[1, 0] == 2
+    assert recs[2, 0] == 1

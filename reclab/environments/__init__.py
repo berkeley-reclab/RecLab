fdf5863df14a59bbee0cb9eca50a5a0f4aa71e95
Karl Krauth <karl.krauth@gmail.com>
2020-03-30 11:26:07 +1100

    Add all recommenders and environments to the appropriate init.py

--- reclab/environments/__init__.py
+++ reclab/environments/__init__.py
@@ -1,4 +1,6 @@
 """The package that contains all environments."""
 from .environment import DictEnvironment
 from .environment import Environment
+from .engelhardt import Engelhardt
+from .latent_factor import LatentFactorBehavior
 from .topics import Topics
--- reclab/recommenders/__init__.py
+++ reclab/recommenders/__init__.py
@@ -1,4 +1,5 @@
 """A set of recommender to be used in conjunction with environments."""
+from .autorec import Autorec
 from .knn_recommender import KNNRecommender
 from .libfm import LibFM
 from .top_pop import TopPop

90cf1160833b5d76cba9635c7b1c57b89e9c1482
Mihaela Curmei <curmeimihaela@gmail.com>
2020-04-03 16:53:07 -0700

    Fix typo

--- reclab/recommenders/llorma/llorma_lib/llorma_g.py
+++ reclab/recommenders/llorma/llorma_lib/llorma_g.py
@@ -418,7 +418,6 @@ class Llorma():
         session = self.session
         model = self.model
 
-        # check th
         predict_k = np.stack(
             [
                 self.anchor_manager.get_k(anchor_idx, user_items)

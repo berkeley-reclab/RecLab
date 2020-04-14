9dfd8a051d18453944171908df251129461282fe
Karl Krauth <karl.krauth@gmail.com>
2020-04-14 17:54:03 +1000

    Tests for slim now test recommendation not prediction.

--- tests/test_slim.py
+++ tests/test_slim.py
@@ -7,7 +7,7 @@ def test_predict():
     """Test that Autorec predicts well and that it gets better with more data."""
     recommender = SLIM(alpha=0.1, l1_ratio=1e-3, seed=0)
     # We want a very high rmse threshold since SLIM doesn't try to drive RMSE down.
-    utils.test_predict_ml100k(recommender, rmse_threshold=3.5)
+    utils.test_binary_recommend_ml100k(recommender, 0.1)
 
 def test_recommend():
     """Test that Autorec will recommend reasonable items."""

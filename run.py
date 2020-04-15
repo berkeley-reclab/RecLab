532568ac7db6537d1c1d237e8d33f151e2fa40b0
Mihaela Curmei <curmeimihaela@gmail.com>
2020-04-14 22:05:17 -0700

    Using LLORMA in run.py

--- run.py
+++ run.py
@@ -11,16 +11,17 @@ from reclab.recommenders.llorma.llorma import Llorma
 from reclab.recommenders import KNNRecommender
 
 def main():
-    # params = {'topic_change': 0.1, 'memory_length': 5,
-    #           'boredom_threshold': 2, 'boredom_penalty': 1.0}
-    # env = Topics(num_topics=5, num_users=100, num_items=100, num_init_ratings=1024, **params)
+    params = {'topic_change': 0.1, 'memory_length': 5, 'rating_frequency': 0.2,
+             'boredom_threshold': 2, 'boredom_penalty': 1.0}
+    env = Topics(num_topics=5, num_users=100, num_items=100, num_init_ratings=1000, **params)
     # params = {'affinity_change': 0.1, 'memory_length': 5,
     #           'boredom_threshold': 0.5, 'boredom_penalty': 1.0}
     # env = LatentFactorBehavior(latent_dim=8, num_users=100, num_items=170, num_init_ratings=1000, **params)
-    env = DatasetLatentFactor('lastfm', num_init_ratings=1000, max_num_users=100, max_num_items=170)
+    # env = DatasetLatentFactor('lastfm', num_init_ratings=1000, max_num_users=100, max_num_items=170)
     # env = RandomPreferences(num_topics=10, num_users=100, num_items=1700, num_init_ratings=10000)
     # recommender = TopPop()
-    recommender = LibFM(num_user_features=0, num_item_features=0, num_rating_features=0, max_num_users=1000, max_num_items=1700)
+    # recommender = LibFM(num_user_features=0, num_item_features=0, num_rating_features=0, max_num_users=1000, max_num_items=1700)
+    recommender = Llorma(use_cache=True)
 
     # First generate the items and users to seed the dataset.
     print("Initializing environment and recommender")
@@ -31,7 +32,7 @@ def main():
     print("Making online recommendations")
     for i in range(100):
         online_users = env.online_users()
-        ret, predicted_ratings = recommender.recommend(online_users, num_recommendations=2, strategy="thompson")
+        ret, predicted_ratings = recommender.recommend(online_users, num_recommendations=1)
         recommendations = ret[:, 0]
         items, users, ratings, info = env.step(recommendations)
         recommender.update(users, items, ratings)

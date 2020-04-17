1dfae31b3c7243e96e3a8d585d28be77a44a7350
sarah <xsarah.dean@gmail.com>
2020-04-17 09:06:42 -0700

    addresssing PR comments

--- reclab/recommenders/baseline.py
+++ reclab/recommenders/baseline.py
@@ -1,6 +1,4 @@
 """An implementation of baseline perfect and random recommenders.
-
-See http://glaros.dtc.umn.edu/gkhome/node/774 for details.
 """
 import numpy as np
 
@@ -19,19 +17,17 @@ class RandomRec(recommender.PredictRecommender):
     """
 
     def __init__(self, rating_range=(0, 5), seed=0):
-        """Create a SLIM recommender."""
+        """Create a random recommender."""
         self._range = rating_range
         np.random.seed(seed)
         super().__init__()
 
     def _predict(self, user_item):  # noqa: D102
         # Random predictions for all pairs.
-        predictions = []
-        for _, _, _ in user_item:
-            predictions.append(np.random.uniform(low=self._range[0],
-                                                 high=self._range[1]))
-
-        return np.array(predictions)
+        predictions = np.random.uniform(low=self._range[0],
+                                        high=self._range[1],
+                                        size=len(user_item))
+        return predictions
 
 
 class PerfectRec(recommender.PredictRecommender):
@@ -44,9 +40,8 @@ class PerfectRec(recommender.PredictRecommender):
     """
 
     def __init__(self, rating_function=0):
-        """Create a SLIM recommender."""
+        """Create a perfect recommender."""
         self._rating_function = rating_function
-        # TODO or shoud it be env._rate_item?? is this mutable?
         super().__init__()
 
     def _predict(self, user_item):  # noqa: D102

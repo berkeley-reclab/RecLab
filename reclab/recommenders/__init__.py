1dcceeb8516597df83c90505a77a3a97c1990448
sarah <xsarah.dean@gmail.com>
2020-05-04 11:39:15 -0700

    updating exploration notebook and graceful faliure for inability to import cfnade

--- reclab/recommenders/__init__.py
+++ reclab/recommenders/__init__.py
@@ -1,6 +1,9 @@
 """A set of recommender to be used in conjunction with environments."""
 from .autorec import Autorec
-from .cfnade import Cfnade
+try:
+	from .cfnade import Cfnade
+except ImportError as e:
+	print('Unable to import CFNade')
 from .knn_recommender import KNNRecommender
 from .libfm import LibFM
 from .llorma import Llorma

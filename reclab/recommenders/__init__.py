a8234e580406e6deef5c5f7c67fe5d687ce89d81
sarah <xsarah.dean@gmail.com>
2020-05-06 14:04:01 -0700

    no need for import check

--- reclab/recommenders/__init__.py
+++ reclab/recommenders/__init__.py
@@ -1,9 +1,6 @@
 """A set of recommender to be used in conjunction with environments."""
 from .autorec import Autorec
-try:
-	from .cfnade import Cfnade
-except ImportError as e:
-	print('Unable to import CFNade')
+from .cfnade import Cfnade
 from .knn_recommender import KNNRecommender
 from .libfm import LibFM
 from .llorma import Llorma

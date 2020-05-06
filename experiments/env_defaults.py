f2ad92833b41314b0b243e4c754a598f322056ee
Mihaela Curmei <curmeimihaela@gmail.com>
2020-05-06 11:33:44 -0700

    Added small exp to the defaults for debug purposes

--- experiments/env_defaults.py
+++ experiments/env_defaults.py
@@ -141,3 +141,139 @@ ML_100K = {
             'boredom_penalty' : 0,
         },
 }
+
+TOPICS_STATIC_SMALL = {
+        'name': 'topics_static_small',
+        'params': {
+            'num_topics' : 5,
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'noise' : 0.5,
+            'topic_change' : 0,
+            'memory_length' : 0,
+            'boredom_threshold'	: 0,
+            'boredom_penalty' : 0,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+            'sampling' : 'uniform',
+        },
+}
+TOPICS_DYNAMIC_SMALL = {
+        'name' : 'topics_dynamic_small',
+        'params': {
+            'num_topics' : 5,
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'noise' : 0.5,
+            'topic_change' : 0.1,
+            'memory_length' : 5,
+            'boredom_threshold'	: 2,
+            'boredom_penalty' : 1,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+            'sampling' : 'uniform',
+        },
+}
+
+LATENT_STATIC_SMALL = {
+        'name' : 'latent_static_small',
+        'params' : {
+            'latent_dim' : 32,
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'noise' : 0.5,
+            'memory_length' : 0,
+            'boredom_threshold'	: 0,
+            'boredom_penalty' : 0,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+            'sampling' : 'uniform',
+        },
+}
+
+LATENT_DYNAMIC_SMALL = {
+        'name' : 'latent_dynamic_small',
+        'params' : {
+            'latent_dim' : 32,
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'noise' : 0.5,
+            'memory_length' : 5,
+            'boredom_threshold'	: 2,
+            'boredom_penalty' : 1,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+            'sampling' : 'uniform',
+        },
+}
+
+SCHMIT_SMALL = {
+        'name' : 'schmit_small',
+        'params' : {
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'rank' : 10,
+            'sigma' : 0.2
+
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+        },
+}
+
+ENGELHARDT_SMALL = {
+        'name' : 'engelhardt_small',
+        'params' : {
+            'num_topics' : 5,
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+            'known_weight' : 0.98,
+            'beta_var' : 1e-05,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+        },
+}
+
+FIXED_SMALL = {
+        'name' : 'fixed_small',
+        'params' : {
+            'num_users' : 100,
+            'num_items' : 170,
+        },
+        'optional_params' : {
+            'rating_frequency' : 0.2,
+            'num_init_ratings' : 1000,
+        },
+        'misc' : {
+            'num_final_ratings' : 2000,
+        },
+}

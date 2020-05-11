eafbae58ce1335df91c1428d23e26dbfe53e102f
sarah <xsarah.dean@gmail.com>
2020-05-11 15:10:38 -0700

    adding scripts for environment flexibility

--- experiments/perfect.py
+++ experiments/perfect.py
@@ -8,8 +8,20 @@ sys.path.append('../../')
 from run_utils import get_env_dataset, run_env_experiment
 from run_utils import ModelTuner
 from reclab.environments import Topics
-from env_defaults import TOPICS_STATIC, get_len_trial
-from reclab.recommenders import TopPop
+from env_defaults import TOPICS_STATIC, TOPICS_DYNAMIC, LATENT_STATIC, LATENT_DYNAMIC, get_len_trial
+from reclab.recommenders import PerfectRec
+
+env_name = str(sys.argv[1])
+if env_name == 'topics_static':
+    ENV_PARAMS = TOPICS_STATIC
+elif env_name == 'topics_dynamic':
+    ENV_PARAMS = TOPICS_DYNAMIC
+elif env_name == 'latent_static':
+    ENV_PARAMS = LATENT_STATIC
+elif env_name == 'latent_dynamic':
+    ENV_PARAMS = LATENT_DYNAMIC
+else:
+    assert False, "environment not implemented!"
 
 # ====Step 4====
 # S3 storage parameters
@@ -20,15 +32,20 @@ overwrite = True
 # Experiment setup.
 n_trials = 10
 trial_seeds = [i for i in range(n_trials)]
-len_trial = get_len_trial(TOPICS_STATIC)
+len_trial = get_len_trial(ENV_PARAMS)
 
 # Environment setup
-environment_name = TOPICS_STATIC['name']
-env = Topics(**TOPICS_STATIC['params'], **TOPICS_STATIC['optional_params'])
+environment_name = ENV_PARAMS['name']
+env = Topics(**ENV_PARAMS['params'], **ENV_PARAMS['optional_params'])
 
 # Recommender setup
-recommender_name = 'TopPop'
-recommender_class = TopPop
+recommender_name = 'PerfectRec'
+recommender_class = PerfectRec
+
+def rating_func(user_id, item_id):
+    ratings = env.dense_ratings
+    return ratings[user_id, item_id]
+
 
 # ====Step 5====
 # Skipping, since nothing to tune
@@ -37,7 +54,7 @@ recommender_class = TopPop
 # Skipping, since nothing to tune
 
 # ====Step 7====
-recommender = recommender_class()
+recommender = recommender_class(rating_func)
 for i, seed in enumerate(trial_seeds):
     run_env_experiment(
             [env],
--- experiments/toppop.py
+++ experiments/toppop.py
@@ -8,8 +8,20 @@ sys.path.append('../../')
 from run_utils import get_env_dataset, run_env_experiment
 from run_utils import ModelTuner
 from reclab.environments import Topics
-from env_defaults import TOPICS_STATIC, get_len_trial
-from reclab.recommenders import PerfectRec
+from env_defaults import TOPICS_STATIC, TOPICS_DYNAMIC, LATENT_STATIC, LATENT_DYNAMIC, get_len_trial
+from reclab.recommenders import TopPop
+
+env_name = str(sys.argv[1])
+if env_name == 'topics_static':
+    ENV_PARAMS = TOPICS_STATIC
+elif env_name == 'topics_dynamic':
+    ENV_PARAMS = TOPICS_DYNAMIC
+elif env_name == 'latent_static':
+    ENV_PARAMS = LATENT_STATIC
+elif env_name == 'latent_dynamic':
+    ENV_PARAMS = LATENT_DYNAMIC
+else:
+    assert False, "environment not implemented!"
 
 # ====Step 4====
 # S3 storage parameters
@@ -20,20 +32,15 @@ overwrite = True
 # Experiment setup.
 n_trials = 10
 trial_seeds = [i for i in range(n_trials)]
-len_trial = get_len_trial(TOPICS_STATIC)
+len_trial = get_len_trial(ENV_PARAMS)
 
 # Environment setup
-environment_name = TOPICS_STATIC['name']
-env = Topics(**TOPICS_STATIC['params'], **TOPICS_STATIC['optional_params'])
+environment_name = ENV_PARAMS['name']
+env = Topics(**ENV_PARAMS['params'], **ENV_PARAMS['optional_params'])
 
 # Recommender setup
-recommender_name = 'PerfectRec'
-recommender_class = PerfectRec
-
-def rating_func(user_id, item_id):
-    ratings = env.dense_ratings
-    return ratings[user_id, item_id]
-
+recommender_name = 'TopPop'
+recommender_class = TopPop
 
 # ====Step 5====
 # Skipping, since nothing to tune
@@ -42,7 +49,7 @@ def rating_func(user_id, item_id):
 # Skipping, since nothing to tune
 
 # ====Step 7====
-recommender = recommender_class(rating_func)
+recommender = recommender_class()
 for i, seed in enumerate(trial_seeds):
     run_env_experiment(
             [env],

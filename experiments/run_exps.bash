f874f5248ad04fb634d7f78f95ce1dce3264893f
sarah <xsarah.dean@gmail.com>
2020-05-13 10:57:26 -0700

    cleaning up some changed files for targeted PR

--- experiments/run_exps.bash
+++
@@ -1,9 +0,0 @@
-python randomrec.py topics_static
-python perfect.py latent_static
-python perfect.py latent_dynamic
-python randomrec.py topics_dynamic
-python randomrec.py latent_static
-python randomrec.py latent_dynamic
-python toppop.py topics_dynamic
-python toppop.py latent_static
-python toppop.py latent_dynamic

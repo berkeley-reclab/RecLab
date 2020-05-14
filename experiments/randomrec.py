b323df10f94a18d90a6cfea29149bc45beff3957
sarah <xsarah.dean@gmail.com>
2020-05-14 10:30:48 -0700

    edit to randomrec script

--- experiments/randomrec.py
+++ experiments/randomrec.py
@@ -31,13 +31,12 @@ else:
 # S3 storage parameters
 bucket_name = 'recsys-eval'
 data_dir = 'master'
-overwrite = False
+overwrite = True
 
 # Experiment setup.
 n_trials = 10
 trial_seeds = [i for i in range(n_trials)]
 len_trial = get_len_trial(ENV_PARAMS)
-len_trial = 2
 print(len_trial)
 # Environment setup
 environment_name = ENV_PARAMS['name']

69247858dc9ed2c025e5511bad0210850723035b
sarah <xsarah.dean@gmail.com>
2020-05-15 12:36:29 -0700

    fixing dynamic init

--- experiments/ease_latent_dynamic.py
+++ experiments/ease_latent_dynamic.py
@@ -8,7 +8,7 @@ sys.path.append('../../')
 from run_utils import get_env_dataset, run_env_experiment
 from run_utils import ModelTuner
 from reclab.environments import Topics, LatentFactorBehavior
-from env_defaults import LATENT_STATIC, get_len_trial
+from env_defaults import LATENT_DYNAMIC, get_len_trial
 from reclab.recommenders import EASE
 
 # ====Step 4====
@@ -20,11 +20,11 @@ overwrite = True
 # Experiment setup.
 n_trials = 10
 trial_seeds = [i for i in range(n_trials)]
-len_trial = get_len_trial(LATENT_STATIC)
+len_trial = get_len_trial(LATENT_DYNAMIC)
 
 # Environment setup
-environment_name = LATENT_STATIC['name']
-env = LatentFactorBehavior(**LATENT_STATIC['params'], **LATENT_STATIC['optional_params'])
+environment_name = LATENT_DYNAMIC['name']
+env = LatentFactorBehavior(**LATENT_DYNAMIC['params'], **LATENT_DYNAMIC['optional_params'])
 
 # Recommender setup
 recommender_name = 'EASE'
--- experiments/ease_topics_dynamic.py
+++ experiments/ease_topics_dynamic.py
@@ -8,7 +8,7 @@ sys.path.append('../../')
 from run_utils import get_env_dataset, run_env_experiment
 from run_utils import ModelTuner
 from reclab.environments import Topics
-from env_defaults import TOPICS_STATIC, get_len_trial
+from env_defaults import TOPICS_STATIC, get_len_trial, TOPICS_DYNAMIC
 from reclab.recommenders import EASE
 
 # ====Step 4====
@@ -20,11 +20,11 @@ overwrite = True
 # Experiment setup.
 n_trials = 10
 trial_seeds = [i for i in range(n_trials)]
-len_trial = get_len_trial(TOPICS_STATIC)
+len_trial = get_len_trial(TOPICS_DYNAMIC)
 
 # Environment setup
-environment_name = TOPICS_STATIC['name']
-env = Topics(**TOPICS_STATIC['params'], **TOPICS_STATIC['optional_params'])
+environment_name = TOPICS_DYNAMIC['name']
+env = Topics(**TOPICS_DYNAMIC['params'], **TOPICS_DYNAMIC['optional_params'])
 
 # Recommender setup
 recommender_name = 'EASE'

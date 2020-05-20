2c1b9483ea5f99b32d069835f148736dfbd54714
sarah <xsarah.dean@gmail.com>
2020-05-19 20:13:15 -0700

    fixing import

--- reclab/environments/__init__.py
+++ reclab/environments/__init__.py
@@ -3,6 +3,6 @@ from .engelhardt import Engelhardt
 from .environment import DictEnvironment
 from .environment import Environment
 from .fixed_rating import FixedRating
-from .latent_factors import LatentFactorBehavior
+from .latent_factors import LatentFactorBehavior, DatasetLatentFactor
 from .schmit import Schmit
 from .topics import Topics

6dec6a8d8eaa39dd507289b54c975245bb5adfa4
Alex Zhao <axyzhao@berkeley.edu>
2020-05-21 09:53:34 -0700

    refactor autorec to pytorch

--- reclab/recommenders/autorec/autorec.py
+++ reclab/recommenders/autorec/autorec.py
@@ -1,92 +1,83 @@
-"""Tensorflow implementation of AutoRec recommender."""
+"""Pytorch implementation of AutoRec recommender."""
+
+import math
 import numpy as np
-import tensorflow as tf
+import torch
 
 from .autorec_lib import autorec
 from .. import recommender
 
-
 class Autorec(recommender.PredictRecommender):
-    """Auto-encoders meet collaborative filtering.
-
-    Parameters
-    ---------
-    num_users : int
-        Number of users in the environment.
-    num_items : int
-        Number of items in the environment.
-    hidden_neuron : int
-        Output dimension of hidden neuron.
-    lambda_value : float
-        Coefficient for regularization while training layers.
-    train_epoch : int
-        Number of epochs to train for each call.
-    batch_size : int
-        Batch size during initial training phase.
-    optimizer_method : str
-        Optimizer for training model; either Adam or RMSProp.
-    grad_clip : bool
-        Set to true to clip gradients to [-5, 5].
-    base_lr : float
-        Base learning rate for optimizer.
-    decay_epoch_step : int
-        Number of epochs before the optimizer decays the learning rate.
-    seed : int
-        Random seed to reproduce results.
-    display_step : int
-        Number of training steps before printing display text.
-
-    """
-
-    def __init__(self,
-                 num_users,
-                 num_items,
-                 hidden_neuron=50,
-                 lambda_value=1,
-                 train_epoch=10,
-                 batch_size=100,
-                 optimizer_method='Adam',
-                 grad_clip=False,
-                 base_lr=1e-4,
-                 decay_epoch_step=50,
-                 seed=0,
-                 display_step=None):
+    def __init__(self, num_users, num_items,
+                 hidden_neuron, lambda_value,
+                 train_epoch, batch_size, optimizer_method,
+                 grad_clip, base_lr, lr_decay,
+                 dropout=0.05, random_seed=0):
         """Create new Autorec recommender."""
         super().__init__()
-        self._hyperparameters.update(locals())
+        self.model = AutoRec(num_users, num_items,
+                             seen_users=set(), seen_items=set(),
+                             hidden_neuron=hidden_neuron,
+                             dropout=dropout, random_seed=random_seed)
+        self.lambda_value = lambda_value
+        self.num_users = num_users
+        self.num_items = num_items
+        self.train_epoch = train_epoch
+        self.batch_size = batch_size
+        self.num_batch = int(math.ceil(self.num_users / float(self.batch_size)))
+        self.base_lr = base_lr
+        self.optimizer_method = optimizer_method
+        self.random_seed = random_seed
 
-        # We only want the function arguments so remove class related objects.
-        del self._hyperparameters['self']
-        del self._hyperparameters['__class__']
+        self.lr_decay = lr_decay
+        self.grad_clip = grad_clip
+        np.random.seed(self.random_seed)
 
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True  # pylint: disable=no-member
-        sess = tf.Session(config=config)
-        seen_users = set()
-        seen_items = set()
-        self.model = autorec.AutoRec(sess,
-                                     num_users,
-                                     num_items,
-                                     None,
-                                     seen_users,
-                                     seen_items,
-                                     hidden_neuron,
-                                     lambda_value,
-                                     train_epoch,
-                                     batch_size,
-                                     optimizer_method,
-                                     grad_clip,
-                                     base_lr,
-                                     decay_epoch_step,
-                                     seed,
-                                     display_step)
+    def train_model(self, data):
+        """
+        Trains for all epochs in train_epoch
+        """
+        self.model.train()
+        if self.optimizer_method == "Adam":
+            optimizer = torch.optim.Adam(self.model.parameters(), lr=self.base_lr)
+
+        elif self.optimizer_method == "RMSProp":
+            optimizer = torch.optim.RMSprop(self.model.parameters(), lr=self.base_lr)
+        else:
+            raise ValueError("Optimizer Key ERROR")
+
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=self.lr_decay)
+
+        for epoch in range(self.train_epoch):
+            self.train(data, optimizer, scheduler)
+
+    def train(self, data, optimizer, scheduler):
+        """Trains for a single epoch"""
+        random_perm_doc_idx = np.random.permutation(self.num_items)
+        for i in range(self.num_batch):
+            if i == self.num_batch - 1:
+                batch_set_idx = random_perm_doc_idx[i * self.batch_size:]
+            elif i < self.num_batch - 1:
+                batch_set_idx = random_perm_doc_idx[i * self.batch_size : (i+1) * self.batch_size]
+
+            output = self.model.forward(data[batch_set_idx, :])
+            loss = self.model._loss(output, data[batch_set_idx, :],
+                             self.mask_R[batch_set_idx, :],
+                             lambda_value=self.lambda_value)
+
+            loss.backward()
+            if self.grad_clip:
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 5)
+
+            optimizer.step()
+            scheduler.step()
 
     @property
     def name(self):  # noqa: D102
         return 'autorec'
 
-    def _predict(self, user_item):  # noqa: D102
-        return self.model.predict(user_item)
+    def _predict(self, user_item):
+        return self.model.predict(user_item, self.R)
 
     def reset(self, users=None, items=None, ratings=None):  # noqa: D102
         self.model.prepare_model()
@@ -97,7 +88,10 @@ class Autorec(recommender.PredictRecommender):
         for user_item in ratings:
             self.model.seen_users.add(user_item[0])
             self.model.seen_items.add(user_item[1])
+
         ratings = self._ratings.toarray()
-        self.model.R = ratings
-        self.model.mask_R = np.clip(ratings, a_min=0, a_max=1)
-        self.model.run()
+        # item-based autorec expects rows that represent items
+        self.R = torch.FloatTensor(ratings.T)
+        self.mask_R = torch.FloatTensor(ratings.T).clamp(0, 1)
+
+        self.train_model(self.R)

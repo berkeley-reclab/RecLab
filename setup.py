64146741b4f19dc02ec633260011671cbe94dac5
Karl Krauth <karl.krauth@gmail.com>
2020-07-17 12:14:41 +1000

    Fix versioning.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='RecLab',
-    version='0.0.1',
+    version='0.1.0',
     author='Karl Krauth',
     author_email='karl.krauth@gmail.com',
     description='A simulation environment for recommender systems.',
-    download_url= 'https://github.com/Karl-Krauth/recsys-eval/archive/v0.0.1.tar.gz',
+    download_url= 'https://github.com/Karl-Krauth/recsys-eval/archive/v0.1.0.tar.gz',
     packages=find_packages(),
 )

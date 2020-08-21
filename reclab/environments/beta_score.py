37d47cd05f0aa8ef735ea2b3c7cf9a9139c67e49
Karl Krauth <karl.krauth@gmail.com>
2020-08-21 13:56:25 +1000

    Implement a complete version of the beta_score environment.

--- reclab/environments/beta_score.py
+++ reclab/environments/beta_score.py
@@ -1,5 +1,5 @@
 """
-Contains the implementation for the Engelhardt environment from the algorithmic confounding paper.
+Contains the implementation for the BetaRank environment from the algorithmic confounding paper.
 
 In this environment users have a hidden preference for each topic and each item has a
 hidden topic assigned to it.
@@ -13,61 +13,7 @@ import scipy.special
 from . import environment
 
 
-class User:
-    """Create custom User object for use in Engelhardt environment."""
-
-    def __init__(self, num_topics, known_weight,
-                 user_topic_weights, beta_var, random):
-        """
-        Initialize user with features and known/unknown utility weight.
-
-        Each user's fraction of known utility is drawn from a beta distribution parameterized by
-        a combination of the same known_weight and beta_var. known_weight
-        and beta_var need to be manipulated
-        before becoming the alpha and beta parameters to each user's distribution.
-
-        Parameters
-        ----------
-        known_weight : float
-            the average fraction of the user's true utility known to the user and the recommender
-
-        user_topic_weights : float array
-            global parameters for each user's topic preferences
-
-        beta_var : int
-            variance of beta distribution
-
-        """
-        self.num_topics = num_topics
-        alpha = ((1 - known_weight) / (beta_var ** 2) - (1 / known_weight)) * (known_weight ** 2)
-        beta = alpha * ((1 / known_weight) - 1)
-        self.known_weight = random.beta(alpha, beta)
-        self.preferences = random.dirichlet(user_topic_weights)
-
-    def rate(self, item_attributes):
-        """
-        Return true utility and known utility from user.
-
-        Returns
-        ------
-        true_util : float
-            User's actual utility, including both known and unknown fractions
-
-        known_util: float
-            User's known utility, found by multiplying true utility
-            by the fraction of utility that is known
-
-        user_dist_choice : str
-            The choice of user distribution for selecting online users. By default, the subset of
-            online users is chosen from a uniform distribution. Currently supports normal
-            and lognormal.
-
-        """
-        true_util = np.dot(self.preferences, item_attributes) * 5
-        return true_util
-
-
-class Engelhardt(environment.DictEnvironment):
+class BetaRank(environment.DictEnvironment):
     """
     Implementation of environment with known and unknown user utility, static over time.
 
@@ -76,27 +22,23 @@ class Engelhardt(environment.DictEnvironment):
 
     """
 
-    def __init__(self, num_topics, num_users, num_items, rating_frequency=0.2,
-                 num_init_ratings=0, known_weight=0.98, beta_var=10 ** -5,
-                 user_dist_choice='uniform'):
-        """Create an Engelhardt environment."""
+    def __init__(self, dimension, num_users, num_items, rating_frequency=0.2,
+                 num_init_ratings=0, known_mean=0.98, user_dist_choice='uniform'):
+        """Create a BetaRank environment."""
         super().__init__(rating_frequency, num_init_ratings, 0, user_dist_choice)
-        self.known_weight = known_weight
-        self.beta_var = beta_var
-        self.user_topic_weights = scipy.special.softmax(self._init_random.rand(num_topics))
-        self.item_topic_weights = scipy.special.softmax(self._init_random.rand(num_topics))
-        self._num_topics = num_topics
-        self._num_users = num_users
-        self._num_items = num_items
-        self._users = None
-        self._users_full = None
-        self._items = None
-        self._ratings = None
-        self._item_attrs = None
+        self._user_preferences = self._init_random.dirichlet(
+            10 * self._init_random.dirichlet(np.ones(dimension)),
+            size=num_users
+        )
+        self._item_preferences = self._init_random.dirichlet(
+            0.1 * self._init_random.dirichlet(100 * np.ones(dimension)),
+            size=num_items
+        )
+        self._known_mean = known_mean
 
     @property
     def name(self):  # noqa: D102
-        return 'engelhardt'
+        return 'beta-rank'
 
     def _get_dense_ratings(self):  # noqa: D102
         ratings = np.zeros([self._num_users, self._num_items])
@@ -118,7 +60,16 @@ class Engelhardt(environment.DictEnvironment):
         self._items = collections.OrderedDict((item_id, np.zeros(0))
                                               for item_id in range(self._num_items))
 
-    def _rate_item(self, user_id, item_id):  # noqa: D102
-        item_attr = self._item_attrs[item_id]
-        rating = self._users_full[user_id].rate(item_attr)
-        return rating
+    def _rate_items(self, user_id, item_ids):  # noqa: D102
+        means = self._item_preferences[item_ids] @ self._user_preferences[user_id]
+        values = self._beta_prime(means)
+        known = self._beta_prime(self._known_mean, shape=len(item_ids))
+        chosen_idx = np.argmax(np.arange(1, len(item_ids) + 1) ** (-0.8) * known * values)
+        ratings = np.ones(len(item_ids)) * np.nan
+        ratings[chosen_idx] = values[chosen_idx]
+        return ratings
+
+    def _beta_prime(mean, std_dev=1e-5):
+        alpha = ((1 - mean) / std_dev ** 2 - 1 / mean) * mean ** 2
+        beta = alpha * (1 / mean - 1)
+        return self._dynamics_random.beta(alpha, beta)

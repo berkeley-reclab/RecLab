cd4d895c1e0a03331bc4839b440a127d94515f01
Karl Krauth <karl.krauth@gmail.com>
2020-08-25 15:48:41 +1000

    Add beta rank tests and fix bugs.

--- reclab/environments/beta_rank.py
+++ reclab/environments/beta_rank.py
@@ -53,15 +53,27 @@ class BetaRank(environment.DictEnvironment):
                                               for item_id in range(self._num_items))
 
     def _rate_items(self, user_id, item_ids):  # noqa: D102
+        # Compute the user's known values for each items and sort them accordingly.
         means = self._item_preferences[item_ids] @ self._user_preferences[user_id]
         values = self._beta_prime(means)
-        known = self._beta_prime(self._known_mean, shape=len(item_ids))
-        chosen_idx = np.argmax(np.arange(1, len(item_ids) + 1) ** (-0.8) * known * values)
+        known = self._beta_prime(self._known_mean, size=len(item_ids))
+        sorted_idxs = reversed(np.argsort(np.arange(1, len(item_ids) + 1) ** (-0.8) *
+                               known * values))
+
+        # Find the index of the item with the highest known value that hasn't been rated yet.
+        chosen_idx = None
+        for idx in sorted_idxs:
+            if (user_id, item_ids[idx]) not in self._ratings:
+                chosen_idx = idx
+                break
+
+        # Rate the chosen item and don't rate anything else.
         ratings = np.ones(len(item_ids)) * np.nan
-        ratings[chosen_idx] = values[chosen_idx]
+        if chosen_idx is not None:
+            ratings[chosen_idx] = values[chosen_idx]
         return ratings
 
-    def _beta_prime(self, mean, std_dev=1e-5, shape=None):
+    def _beta_prime(self, mean, std_dev=1e-5, size=None):
         alpha = ((1 - mean) / std_dev ** 2 - 1 / mean) * mean ** 2
         beta = alpha * (1 / mean - 1)
-        return self._dynamics_random.beta(alpha, beta, shape=shape)
+        return self._dynamics_random.beta(alpha, beta, size=size)

68718dc52c7cccb5fd89b7f0c8df156ba7b3f4b3
Karl Krauth <karl.krauth@gmail.com>
2020-09-05 15:34:59 +1000

    Add requirements to setup.py

--- reclab/recommenders/__init__.py
+++ reclab/recommenders/__init__.py
@@ -1,10 +1,14 @@
 """A set of recommender to be used in conjunction with environments."""
-from .autorec import Autorec
-from .cfnade import Cfnade
 from .knn_recommender import KNNRecommender
 from .libfm import LibFM
-from .llorma import Llorma
-from .sparse import SLIM, EASE
 from .top_pop import TopPop
 from .baseline import RandomRec
 from .baseline import PerfectRec
+
+try:
+    from .autorec import Autorec
+    from .cfnade import Cfnade
+    from .llorma import Llorma
+    from .sparse import SLIM, EASE
+except ImportError:
+    pass

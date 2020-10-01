682ca9092ff097aab388e7c1dae5cdd809461510
Mihaela Curmei <curmeimihaela@gmail.com>
2020-10-01 14:31:48 -0700

    typo fix

--- tests/test_llorma.py
+++ tests/test_llorma.py
@@ -26,18 +26,18 @@ def test_llorma_predict():
 def test_llorma_recommend():
     """Test that LLORMA will recommend reasonable items."""
     recommender = Llorma(max_user=utils.NUM_USERS_ML100K,
-                        max_item=utils.NUM_ITEMS_ML100K,
-                        n_anchor=10,
-                        pre_rank=10,
-                        pre_learning_rate=3e-4,
-                        pre_lambda_val=0.01,
-                        pre_train_steps=70,
-                        rank=20,
-                        learning_rate=2e-2,
-                        lambda_val=1e-4,
-                        train_steps=50,
-                        batch_size=1000,
-                        use_cache=False,
-                        result_path='results',
-                        random_seed=0)
+                         max_item=utils.NUM_ITEMS_ML100K,
+                         n_anchor=10,
+                         pre_rank=10,
+                         pre_learning_rate=3e-4,
+                         pre_lambda_val=0.01,
+                         pre_train_steps=70,
+                         rank=20,
+                         learning_rate=2e-2,
+                         lambda_val=1e-4,
+                         train_steps=50,
+                         batch_size=1000,
+                         use_cache=False,
+                         result_path='results',
+                         random_seed=0)
     utils.test_recommend_simple(recommender)
